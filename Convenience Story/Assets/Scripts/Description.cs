c416a0f174831e6bcc8489a27f6d6d0cf9bab375
Monshta <frgimena@gmail.com>
2015-06-15 19:21:36 -0700

    v0.12

--- Convenience Story/Assets/Scripts/Description.cs
+++ Convenience Story/Assets/Scripts/Description.cs
@@ -6,23 +6,22 @@ using System.Collections;
 public class Description : MonoBehaviour {
 	private GameObject Swag;
 	private Main main;
-	Text buttonText;
+	public Text descriptionText;
 	// Use this for initialization
 	void Start () {
-		buttonText = GetComponent<Text> ();
 		Swag = GameObject.Find ("Swag");
 		main = Swag.GetComponent<Main> ();
 	}
 	public void storeUpgradeMsg(string S){
-		buttonText.text = "Cost : $" + main.storeUpgradeCost +
+		descriptionText.text = "Cost : $" + main.storeUpgradeCost +
 			"\n increase customer rate by 2 and fame by 2";
 	}
 	public void merchandiceUpgradeMsg(string M){
-		buttonText.text = "Cost : $" + main.maintaneinceCosts +
+		descriptionText.text = "Cost : $" + main.maintaneinceCosts +
 			"\n increase price by $1.50 and fame by 2";
 	}
 	public void advertisementUpgradeMsg( string A){
-		buttonText.text = "Cost : $" + main.advertisementCost +
+		descriptionText.text = "Cost : $" + main.advertisementCost +
 			"\ninccrease fame by " + (main.fame * 2);
 	}
 
